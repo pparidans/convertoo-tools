85e34ee6f36fd080a50f398c131f8c03bf32faef
Pierre Paridans <pierre@paridans.org>
2016-05-12 14:34:42 +0200

    added Bootstrap & layout/styling

--- tva-com-stru/src/components/app.js
+++ tva-com-stru/src/components/app.js
@@ -7,25 +7,29 @@ const App = React.createClass({
       enterpriseNumber: ''
     }
   },
+
   render() {
     const { enterpriseNumber } = this.state
     const comStructured = convertVat(enterpriseNumber)
     return (
       <div className="container">
-        <div className="row">
-          <form>
-            <fieldset class="form-group">
-              <label htmlFor="enterprise-number">Numéro d'entreprise</label>
-              <input type="text" id="enterprise-number" value={enterpriseNumber} onChange={this.setEnterpriseNumber} placeholder="BE00.0000.0000" className="form-control" maxLength="14" />
-            </fieldset>
-          </form>
-        </div>
-        <div className="row">
-          <p className="alert alert-info">+++ {comStructured} +++</p>
+        <div className="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+          <div className="row">
+            <form>
+              <fieldset class="form-group">
+                <label htmlFor="enterprise-number" className="control-label">Numéro d'entreprise</label>
+                <input type="text" id="enterprise-number" value={enterpriseNumber} onChange={this.setEnterpriseNumber} placeholder="BE00.0000.0000" className="form-control input-lg" maxLength="14" />
+              </fieldset>
+            </form>
+          </div>
+          <div className="row result">
+            <p className="alert alert-info text-center">+++ {comStructured} +++</p>
+          </div>
         </div>
       </div>
     )
   },
+
   setEnterpriseNumber(ev) {
     const { target: { value = '' } } = ev
     this.setState({ enterpriseNumber: value })
--- tva-com-stru/src/main.js
+++ tva-com-stru/src/main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app'
+import './stylesheets/styles.css'
 
 const appContainer = document.getElementById('root')
 
